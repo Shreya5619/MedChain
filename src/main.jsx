96532c170892c9385f20d6755922fe14eb96412b
CallmeSharanya <sharanyamadhyastha25@gmail.com>
2024-12-17 20:47:58 +0530

    Manufacturer frontend

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Manufacturer from './Manufacturer.jsx';
+import './index.css'; // Tailwind CSS imports
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Manufacturer />
+  </React.StrictMode>
+);
