446c3943a807f27a0dc2aa1ff0d89cb146579f9c
Shreya Prasad <shreyaprasad019@gmail.com>
2024-12-18 13:46:15 +0530

    intermediary routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter, Route,Routes } from 'react-router-dom'
-import ManufacturerDashboard from './Manufacturer.jsx'
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ManufacturerDashboard from './Manufacturer.jsx';
+import IntermediaryDashboard from './Intermediary.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<ManufacturerDashboard />}/>
-    </Routes>
-    </BrowserRouter>
-    </>
-  )
+    <div className="h-full">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/manufacturer' element={<ManufacturerDashboard />} />
+          <Route path='/intermediary' element={<IntermediaryDashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
